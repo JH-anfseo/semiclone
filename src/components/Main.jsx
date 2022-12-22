d5079ee1319398841d67d77ef5553795b0e91ad2
GoMyamMii <sun302700@naver.com>
2022-12-22 16:24:56 +0900

    initial setting
    기본 골자가 되는 컴포넌트를 만들었습니다.
    
    by @GoMyamMii

---
+++ src/components/Main.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import TodoList from "./TodoList";
+
+const Main = () => {
+  return (
+    <>
+      <button></button>
+      <TodoList />
+    </>
+  );
+};
+
+export default Main;
---
+++ src/components/TodoList.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Comment from "./Comment";
+import Todo from "./Todo";
+
+const TodoList = () => {
+  return (
+    <>
+      <Todo />
+      <Comment />
+    </>
+  );
+};
+
+export default TodoList;
